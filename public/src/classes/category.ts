1093c961ffa9524973865c479174359e420411dc
YannouLR <yann.lenerand@yahoo.com>
2022-02-24 11:00:48 +0100

    classes category finish

---
+++ public/src/classes/category.ts
@@ -0,0 +1,25 @@
+export class Category {
+    #id : number;
+    #name:string;
+    #isdeleted:boolean;
+
+    constructor(id?: number, name?: string, isdeleted?: boolean) {
+        this.#id = id ?? 0;
+        this.#name = name ?? "";
+        this.#isdeleted = isdeleted ?? false;
+    }
+
+    get id() { return this.#id; }
+    get name() { return this.#name; }
+    get isdeleted() { return this.#isdeleted; }
+
+    set id(id:number) { this.#id = id;}
+    set name(name:string) { this.#name = name}
+    set isdeleted(isdeleted:boolean) { this.#isdeleted = isdeleted}
+
+    createArticle(){}
+
+    fetchArticle(id:number){}
+
+    deleteArticle(id:number){}
+}
