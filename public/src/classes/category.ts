425a778fe67263a97777804b3802760930a489cd
Clemy-beep <clementine.digny@gmail.com>
2022-02-24 14:04:29 +0100

    tmp

--- public/src/classes/category.ts
+++ public/src/classes/category.ts
@@ -1,20 +1,20 @@
 export class Category {
     #id: number;
-    #name: string;
+    #label: string;
     #isdeleted: boolean;
 
     constructor(id?: number, name?: string, isdeleted?: boolean) {
         this.#id = id ?? 0;
-        this.#name = name ?? "";
+        this.#label = name ?? "";
         this.#isdeleted = isdeleted ?? false;
     }
 
     get id() { return this.#id; }
-    get name() { return this.#name; }
+    get label() { return this.#label; }
     get isdeleted() { return this.#isdeleted; }
 
     set id(id: number) { this.#id = id; }
-    set name(name: string) { this.#name = name }
+    set label(name: string) { this.#label = name }
     set isdeleted(isdeleted: boolean) { this.#isdeleted = isdeleted }
 
     createCategory(label: string) { }
